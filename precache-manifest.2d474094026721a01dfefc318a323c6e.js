0bf20dc12574fa4273ad2a63a5e23d9db009ce79
Marwan F <marwan.mm@live.com>
2020-01-16 09:22:43 +1000

    Deploy

---
+++ precache-manifest.2d474094026721a01dfefc318a323c6e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a123f7caa9583c44b55bbd3ee805c764",
+    "url": "/index.html"
+  },
+  {
+    "revision": "26af5dc7d7c577978be7",
+    "url": "/static/css/main.b9881d97.chunk.css"
+  },
+  {
+    "revision": "1a3f7d15f75d4221e7df",
+    "url": "/static/js/2.550e7828.chunk.js"
+  },
+  {
+    "revision": "0c08a48b164499df78c0607b14eb2c76",
+    "url": "/static/js/2.550e7828.chunk.js.LICENSE"
+  },
+  {
+    "revision": "26af5dc7d7c577978be7",
+    "url": "/static/js/main.0624205d.chunk.js"
+  },
+  {
+    "revision": "b8af09b55ed0c52ee8e5",
+    "url": "/static/js/runtime-main.7fba1cad.js"
+  }
+]);
